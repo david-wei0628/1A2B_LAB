1bf4f2d9e50258ae705d450100a97cea43769ac4
魏 大維 <d46829173@gmail.com>
2021-11-07 14:32:11 +0800

    加入專案檔案。

---
+++ 1A2B_LAB/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace _1A2B_LAB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("UD Digi Kyokasho NK-B", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(620, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.startbt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("UD Digi Kyokasho NK-B", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "1A2B";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(30, 135);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(707, 279);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "0A0B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(425, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 40);
+            this.label4.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 40);
+            this.label3.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("UD Digi Kyokasho NK-B", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(40, 70);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(40, 35);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("UD Digi Kyokasho NK-B", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(115, 70);
+            this.textBox2.MaxLength = 1;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(40, 35);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("UD Digi Kyokasho NK-B", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(190, 70);
+            this.textBox3.MaxLength = 1;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(40, 35);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("UD Digi Kyokasho NK-B", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(620, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 55);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "RES";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.resbt_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("UD Digi Kyokasho NK-B", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(460, 85);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 55);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Lock";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Lockbt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ 1A2B_LAB/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1A2B_LAB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        int[] com = new int[10];
+        int[] player1 = new int[3];
+        int Ax;
+        int Bx;
+
+        void AB_lab()
+        {
+            int[] x = new int[3];
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+            {
+                x[0] = Int32.Parse(textBox1.Text);
+                x[1] = Int32.Parse(textBox2.Text);
+                x[2] = Int32.Parse(textBox3.Text);
+                for (int i = 0; i < 3; i++)
+                {
+                    if (player1[i] == x[0] && i != 0) { Bx++; }
+                    else if (player1[i] == x[0] && i == 0) { Ax++; }
+                    if (player1[i] == x[1] && i != 1) { Bx++; }
+                    else if (player1[i] == x[1] && i == 1) { Ax++; }
+                    if (player1[i] == x[2] && i != 2) { Bx++; }
+                    else if (player1[i] == x[2] && i == 2) { Ax++; }
+                }
+            }
+            if(Ax == 3) { label4.ForeColor = System.Drawing.Color.Red; }
+            else if(Ax >0) { label4.ForeColor = System.Drawing.Color.DarkOrange; }
+            else if(Bx > 0) { label4.ForeColor = System.Drawing.Color.Blue; }
+            else { label4.ForeColor = System.Drawing.Color.Black; }
+        }
+        /// <summary>
+        /// 元件狀態統一變化
+        /// </summary>
+        void element_Enab()
+        {
+            button1.Enabled = !button1.Enabled;
+            button2.Enabled = !button2.Enabled;
+            button3.Enabled = !button3.Enabled;
+            textBox1.Enabled = !textBox1.Enabled;
+            textBox2.Enabled = !textBox2.Enabled;
+            textBox3.Enabled = !textBox3.Enabled;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            label4.ForeColor = System.Drawing.Color.Black;
+        }
+
+        private void startbt_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            int x;
+            Random rnd = new Random();
+            for (int i = 0; i<3 ; i++)
+            {
+                while(true)
+                {
+                    x = rnd.Next(1, 9);
+                    if(com[x] == 0)
+                    { 
+                        com[x] = 1;
+                        break;
+                    }
+                }
+                player1[i] = x;
+                label1.Text = label1.Text + player1[i].ToString();
+            }
+            element_Enab();
+        }
+
+        private void resbt_Click(object sender, EventArgs e)
+        {
+            if (!button1.Enabled)
+            { element_Enab(); }
+            for(int i=0;i<=9;i++)
+            { com[i] = 0; }
+            for(int i=0;i<3;i++)
+            { player1[i] = 0; }
+            label1.Text = "1A2B";
+            label3.Text = "";
+            label4.Text = "";
+        }
+
+        private void Lockbt_Click(object sender, EventArgs e)
+        {
+            
+            AB_lab();
+            lab_update();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        /// <summary>
+        /// label 刷新
+        /// </summary>
+        void lab_update()
+        {
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "" )
+            {
+                label3.Text = label3.Text + textBox1.Text + textBox2.Text + textBox3.Text + "\r\n";
+                label4.Text = label4.Text + Ax + "A" + Bx + "B" + "\r\n";
+                groupBox1.Text = Ax + "A" + Bx + "B";
+                Ax = 0;
+                Bx = 0;
+            }
+        }
+    }
+}
